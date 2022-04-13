8d93336626b789f0211b574aaea3907eed7c04e4
iKarans <68602829+iKarans@users.noreply.github.com>
2022-04-13 16:27:56 +0100

    done

--- cypress/components/Form.spec.tsx
+++ cypress/components/Form.spec.tsx
@@ -16,7 +16,8 @@ describe("Testing App" , () => {
         cy.get('[data-cy=username-input]').type('karan12345')
         cy.get('[data-cy=password-input]').type('karan12345')
         cy.get('[data-cy=submit-btn]').click()
-        cy.get('[data-cy=success-message]').should('exist');
+        cy.get('[data-cy=success-message]').should('exist').contains("User is valid");
+        cy.get('form').should('not.exist');
     })
     it("should not submit if meets validation", () => {
         mount(<Form />);
@@ -24,5 +25,6 @@ describe("Testing App" , () => {
         cy.get('[data-cy=password-input]').type('karan12345')
         cy.get('[data-cy=submit-btn]').click()
         cy.get('[data-cy=success-message]').should('not.exist');
+        cy.get('form').should('exist');
     })
 })
